49e10890674bfb309a1b45d829a70bf9b11374e3
IrynaBuchma <buchma.iryna2018@gmail.com>
2023-04-30 15:02:07 +0300

    copy hw to new repo because of deploy bug

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,71 @@
-export const App = () => {
-  return (
-    <div
+import {Component} from "react";
+import Statistics from './Statistics/Statistics';
+import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
+import Section from './Section/Section';
+import Notification from "./Notification/Notification";
+
+export class App extends Component {
+  
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+  }
+
+  addStatisticValue = prop => {
+    this.setState(prevState => ({
+      [prop]: prevState[prop] + 1
+    }))
+  }
+
+  countTotalFeedback = () => {
+    return Object.values(this.state).reduce((acc, prop) => acc + prop, 0 ); 
+  }
+
+  countPositiveFeedbackPercentage() {
+    const{ good } = this.state;
+    return Math.round((good * 100) / this.countTotalFeedback()) || 0;
+  } 
+
+  render() {
+
+    const { good, neutral, bad} = this.state;
+    const total = this.countTotalFeedback();
+    const positiveFeedbackPercentage = this.countPositiveFeedbackPercentage();
+
+    return (
+      <div
       style={{
+        marginBottom: 20,
+        padding: 20,
         height: '100vh',
         display: 'flex',
+        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
-    >
-      React homework template
-    </div>
-  );
+      > 
+        <Section title="Please leave feedback">
+            <FeedbackOptions 
+                options={Object.keys(this.state)} 
+                onLeaveFeedback={this.addStatisticValue}></FeedbackOptions>
+        </Section>
+        <Section title="Statistics">
+          {this.countTotalFeedback() ? (
+          <Statistics
+            good={good} 
+            neutral={neutral} 
+            bad={bad} 
+            total={total} 
+            positiveFeedbackPercentage={positiveFeedbackPercentage}></Statistics>
+          )
+            : (
+          <Notification message="There is no feedback"></Notification>
+          )}
+        </Section>
+      </div>   
+    );
+  }
 };
